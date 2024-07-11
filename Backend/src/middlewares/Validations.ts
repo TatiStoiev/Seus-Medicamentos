96398467a3d03254f324edc5622e25da7b9b6f32
TatiStoiev <tatistoiev@hotmail.com>
2024-07-11 15:22:29 -0300

    refactor: add new validations

--- Backend/src/middlewares/Validations.ts
+++ Backend/src/middlewares/Validations.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 class Validations {
+
     static validateMedicamento(req: Request, res: Response, next: NextFunction): Response | void {
         const medicamento = req.body;
         const requestedKeys = ['nome', 'principioAtivo', 'apresentacao', 'uso', 'interacoesMedicamentosas'];
@@ -8,6 +9,33 @@ class Validations {
         if (notFoundKey) {
             return res.status(400).json( { message: `O campo ${notFoundKey} é necessário` })
         }
+        const emptyKey = requestedKeys.find((key) => medicamento[key].trim() === '' );
+        if (emptyKey) {
+            return res.status(400).json( {
+                 message: `O campo ${emptyKey} não pode estar vazio`
+            });
+        }
+
+        next();
+    }
+
+    static validateSearchByName(req: Request, res: Response, next: NextFunction): Response | void {
+
+        const { nome } = req.query;
+        if (nome === '') {
+            return res.status(400).json( { message: 'O campo nome deve estar preenchido' });
+        }
+
+        next();
+    }
+
+    static validateSearchByPrincipioAtivo(req: Request, res: Response, next: NextFunction): Response | void {
+
+        const { principioAtivo } = req.query;
+        if (principioAtivo === '') {
+            return res.status(400).json( { message: 'O campo principioAtivo deve estar preenchido' });
+        }
+
         next();
     }
 }
